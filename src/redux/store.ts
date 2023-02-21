f9467ffdca31c119fd38b5c9a42dcac033e2b964
Leonel_cyan <quue1988@hotmail.com>
2023-02-21 18:01:05 +0800

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,12 +7,14 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "@reduxjs/toolkit";
 
+// 配置 persist
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
 
+// persist 需要使用combineReducer
 const persistedReducer = persistReducer(
   persistConfig,
   combineReducers({
@@ -21,10 +23,11 @@ const persistedReducer = persistReducer(
   })
 );
 
+// 改造 store
 export const store = configureStore({
   // 指定reducer
   reducer: persistedReducer,
-  // 這裏使用中間件
+  // 這裏使用中間件，關閉 serializableCheck 防止出錯
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
@@ -33,6 +36,6 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
-// 類型定義
+// TS類型定義
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
