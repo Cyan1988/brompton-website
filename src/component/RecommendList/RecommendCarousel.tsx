e9b40a0db14726f51013b449f15112803052ae67
Leonel_cyan <quue1988@hotmail.com>
2023-02-24 00:56:00 +0800

    update

--- src/component/RecommendList/RecommendCarousel.tsx
+++ src/component/RecommendList/RecommendCarousel.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Carousel, Card } from "antd";
 import products_list from "../../data/products_list.json";
-import "./arrow_style.css";
+import { LeftOutlined } from "@ant-design/icons";
+import { RightOutlined } from "@ant-design/icons/lib/icons";
+// import "./arrow_style.css";
 
 const itemList: {
   title: string;
@@ -14,28 +16,59 @@ const itemList: {
 // 卡片组件
 const { Meta } = Card;
 
-// 輪播思考重構中
-
 // 响应式的卡片轮播图
-const RecommendCarousel: React.FC = () => (
-  <>
-    <div className="hidden xl:block">
+const RecommendCarousel: React.FC = () => {
+  // useRef 傳參
+  const carouselRef: any = useRef(null);
+
+  return (
+    <>
       <Carousel
         autoplay
-        arrows={true}
+        autoplaySpeed={5000}
+        // arrows={true}
         dots={false}
-        slidesToShow={4}
-        slidesToScroll={4}
-        className="content-around px-12 bg-zinc-600"
+        responsive={[
+          {
+            breakpoint: 2048,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+            },
+          },
+          {
+            breakpoint: 1280,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            },
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ]}
+        className="content-around px-12 pb-2 bg-zinc-600"
+        ref={carouselRef}
       >
         {itemList.map((item) => (
           <div key={item.title}>
             <Card
-              className="block h-[30rem] mx-4 my-6 cursor-pointer"
+              className="block h-[25rem] md:h-[30rem] mx-4 my-6 cursor-pointer"
               hoverable
               cover={
                 <img
-                  className="h-[20rem] object-cover"
+                  className="h-[15rem] md:h-[20rem] object-cover"
                   alt=""
                   src={item.img_url}
                 />
@@ -52,201 +85,21 @@ const RecommendCarousel: React.FC = () => (
           </div>
         ))}
       </Carousel>
-      {/* 先用组件库内置函数和css代替一下，以后再自己写 */}
-      <div className="absolute flex w-full -translate-y-[17.7rem] ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block translate-x-3 h-10 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block h-10 ml-auto mr-2 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
-      </div>
-    </div>
-    <div className="hidden lg:block xl:hidden">
-      <Carousel
-        autoplay
-        arrows={true}
-        dots={false}
-        slidesToShow={3}
-        slidesToScroll={4}
-        className="content-around px-12 bg-zinc-600"
-      >
-        {itemList.map((item) => (
-          <div key={item.title}>
-            <Card
-              className="block h-[30rem] mx-4 my-6 cursor-pointer"
-              hoverable
-              cover={
-                <img
-                  className="h-[20rem] object-cover"
-                  alt=""
-                  src={item.img_url}
-                />
-              }
-            >
-              <Meta title={item.title} />
-              <div className="mx-1 my-6">
-                {item.price}
-                <span className="px-2">—</span>
-                {item.weight}
-              </div>
-              <Meta description={item.text}></Meta>
-            </Card>
-          </div>
-        ))}
-      </Carousel>
-      {/* 先用组件库内置函数和css代替一下，以后再自己写 */}
-      <div className="absolute flex w-full -translate-y-[17.7rem] ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block translate-x-3 h-10 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block h-10 ml-auto mr-2 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
-      </div>
-    </div>
-    <div className="hidden md:block lg:hidden">
-      <Carousel
-        autoplay
-        arrows={true}
-        dots={false}
-        slidesToShow={2}
-        slidesToScroll={6}
-        className="content-around px-12 bg-zinc-600"
-      >
-        {itemList.map((item) => (
-          <div key={item.title}>
-            <Card
-              className="block h-[30rem] mx-4 my-6 cursor-pointer"
-              hoverable
-              cover={
-                <img
-                  className="h-[20rem] object-cover"
-                  alt=""
-                  src={item.img_url}
-                />
-              }
-            >
-              <Meta title={item.title} />
-              <div className="mx-1 my-6">
-                {item.price}
-                <span className="px-2">—</span>
-                {item.weight}
-              </div>
-              <Meta description={item.text}></Meta>
-            </Card>
-          </div>
-        ))}
-      </Carousel>
-      {/* 先用组件库内置函数和css代替一下，以后再自己写 */}
-      <div className="absolute flex w-full -translate-y-[17.7rem] ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block translate-x-3 h-10 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="inline-block h-10 ml-auto mr-2 text-white cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
-      </div>
-    </div>
-    <div className="block md:hidden">
-      <Carousel
-        autoplay
-        arrows={true}
-        slidesToShow={1}
-        slidesToScroll={1}
-        className="content-around px-12 bg-zinc-600"
-      >
-        {itemList.map((item) => (
-          <div key={item.title}>
-            <Card
-              className="block h-[20rem] mx-6 mb-6 cursor-pointer"
-              hoverable
-              cover={
-                <img
-                  className="h-[11rem] object-cover"
-                  alt=""
-                  src={item.img_url}
-                />
-              }
-            >
-              <Meta title={item.title} />
-              <div className="mx-1 my-6">
-                {item.price}
-                <span className="px-2">—</span>
-                {item.weight}
-              </div>
-              <Meta description={item.text}></Meta>
-            </Card>
-          </div>
-        ))}
-      </Carousel>
-    </div>
-  </>
-);
+
+      {/* 左右切換按鈕 */}
+      <LeftOutlined
+        className="text-white absolute text-[2rem] left-2 -translate-y-[19rem]"
+        onClick={() => {
+          carouselRef.current.prev();
+        }}
+      />
+      <RightOutlined
+        className="text-white absolute text-[2rem] right-2 -translate-y-[19rem]"
+        onClick={() => {
+          carouselRef.current.next();
+        }}
+      />
+    </>
+  );
+};
 export default RecommendCarousel;
--- src/component/RecommendList/RecommendListTitle.tsx
+++ src/component/RecommendList/RecommendListTitle.tsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 
 function RecommendListTitle() {
   return (
-    <div className="text-center text-[2.7rem] md:text-[4rem] lg:text-[5rem] py-3 text-white bg-zinc-600">
+    <div className="text-center text-[2.7rem] md:text-[4rem] lg:text-[5rem]  text-white bg-zinc-600 pt-2">
       BROMPTON BIKES
-      <div className="flex my-3">
+      <div className="flex">
         <button className="text-[1rem] px-2 bg-transparent border-2 border-white rounded ml-auto mr-[4rem]">
           <NavLink to="/shop">Show All</NavLink>
         </button>
